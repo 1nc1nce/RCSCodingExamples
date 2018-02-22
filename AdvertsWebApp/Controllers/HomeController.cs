81de2ae99cfdb999c9100c07b2218107c16d281e
Ineta <ineta.ofmane@gmail.com>
2018-02-22 21:25:17 +0200

    Updated AdvertsWebApp

--- AdvertsWebApp/Controllers/HomeController.cs
+++ AdvertsWebApp/Controllers/HomeController.cs
@@ -10,9 +10,34 @@ namespace AdvertsWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        //Konstruktors
+        public HomeController()
+        {
+            adverts = new List<Advert>();
+            Advert ad = new Advert(); // Nepieciešams pievienot using AdvertsWebApp.Models
+            ad.AdvertID = 1;
+            ad.Name = "BMW";
+            ad.AdvertText = "Šis ir labs BMW";
+            ad.Price = 2000.95;
+            ad.CreationTime = DateTime.Now;
+
+            Advert homeAd = new Advert(); 
+            homeAd.AdvertID = 2;
+            homeAd.Name = "Māja";
+            homeAd.AdvertText = "Liela māja";
+            homeAd.Price = 12000;
+            homeAd.CreationTime = new DateTime(1999,12,31);
+
+            adverts.Add(ad);
+            adverts.Add(homeAd);
+        }
+        private List<Advert> adverts; //DB imitācija, saraksts
+
+        //Šī f-ja tiek izsaukta, kad tiek pieprasīts weblapas bāzes ceļš
         public IActionResult Index()
         {
-            return View();
+            //Izsaucam view f-ju, lai uzģenerētu html rezultātu no Index.cshtml faila, tajā iekšā izmantojot datus, kas pieejami adverts sarakstā
+            return View(adverts);
         }
 
         public IActionResult About()
@@ -32,6 +57,18 @@ namespace AdvertsWebApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Advert(int advertID)
+        {
+            foreach (var ad in adverts)
+            {
+                if (ad.AdvertID == advertID) // meklē sludinājumu, kuram sakrīt ID
+                {
+                    return View(ad);
+                }
+            }
+            return View();
         }
     }
 }
---
+++ AdvertsWebApp/Models/Advert.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace AdvertsWebApp.Models
+{
+    public class Advert
+    {
+        public Advert()
+        {
+        }
+        public int AdvertID { get; set; }
+        public double Price { get; set; } //Mainīgajiemm nepieciešams norādīt properties ar get, set
+        public string Name { get; set; }
+        public string AdvertText { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
